b97ff9c8e4c7abceadcbf7ff3cca5e2156d0a63f
Mohit <themohitnair@protonmail.com>
2024-12-14 10:30:40 +0530

    changed footer style

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
     return (
-        <footer className="sticky bottom-0 bg-card text-gray-400 py-4 text-center flex justify-between px-4 items-center">
+        <footer className="sticky bottom-0 bg-card text-gray-400 py-2 text-center flex justify-between px-10 items-center">
             <p className="text-sm">
                 Developed by <a href="https://github.com/themohitnair" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">Mohit Nair</a>
             </p>
